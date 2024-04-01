5bdf8b541c755bd356e8fd0db49eb808eaaf8a3c
urosjarc <jar.fmf@gmail.com>
2024-04-02 01:46:59 +0200

    Adding frontend

---
+++ app/src/routes/user/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {Button, Container, FormGroup, Input} from "@sveltestrap/sveltestrap";
+
+    function login() {
+        alert(email)
+    }
+
+    let email = ""
+    let password = ""
+</script>
+
+<Container sm>
+    <h1>User</h1>
+    <FormGroup>
+        <Input bind:value={email} type="email" placeholder="email placeholder"/>
+        <Input bind:value={password} type="password" placeholder="password placeholder"/>
+        <Button block on:click={login}>Login</Button>
+    </FormGroup>
+</Container>
